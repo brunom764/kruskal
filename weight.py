38eefc90d44b2e94ee20f3e5323641845865c028
Bruno Miguel Moreira Albuquerque <brunom764@gmail.com>
2023-04-13 11:48:21 -0300

    refactor: Optimize algorithm

--- weight.py
+++ weight.py
@@ -1,31 +1,22 @@
+# Função para modificar os pesos da base de dados,
+# pois todos estão com peso 1, agora será gerado
+# um peso de numero aleatório entre 1 e 184 e será
+# armazenado essa alteração no arquivo jazzNew.net
+
 import random
 
-with open('jazz.net') as f:
+with open('jazz.net') as f:   # abrir arquivo original
     lines = f.readlines()
 
-# guardar numero de vertices
-_, nodes = lines[0].split()
-
-# Criar grafo
-graph = {}
-for line in lines:
-    if line.startswith('*') or not line.strip():  # verifica se a linha está vazia ou começa com *
-        continue
-
-    values = line.rstrip().split()
-    if len(values) < 3: # verifica se a linha contém pelo menos 3 valores
-        continue
-
-    src, desc, weight = values
-    graph.setdefault(src, {})[desc] = weight
-
+_, nodes = lines[0].split()  # guardar numero de vertices
 
 with open('jazzNew.net', 'w') as f:
-    f.write(f"*{len(graph)} {nodes}\n")  # escreve o cabeçalho do arquivo
+    f.write(f"*5484 {nodes}\n")  # escreve o cabeçalho do arquivo
     for line in lines:
         if line.startswith('*'):
             continue
-        src, desc, _ = line.rstrip().split()
+        src, desc, _ = line.rstrip().split()   # Guarda as variaveis
         weight = random.randint(1,184)
         temp = [src, desc, str(weight)]
         f.write(" ".join(temp) + "\n")
+
