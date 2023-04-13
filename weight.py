d9ef75439d2ee95bcf3b5035895fa5abf5795381
Bruno Moreira <100159869+brunom764@users.noreply.github.com>
2023-04-13 11:20:29 -0300

    Add files via upload

---
+++ weight.py
@@ -0,0 +1,31 @@
+import random
+
+with open('jazz.net') as f:
+    lines = f.readlines()
+
+# guardar numero de vertices
+_, nodes = lines[0].split()
+
+# Criar grafo
+graph = {}
+for line in lines:
+    if line.startswith('*') or not line.strip():  # verifica se a linha está vazia ou começa com *
+        continue
+
+    values = line.rstrip().split()
+    if len(values) < 3: # verifica se a linha contém pelo menos 3 valores
+        continue
+
+    src, desc, weight = values
+    graph.setdefault(src, {})[desc] = weight
+
+
+with open('jazzNew.net', 'w') as f:
+    f.write(f"*{len(graph)} {nodes}\n")  # escreve o cabeçalho do arquivo
+    for line in lines:
+        if line.startswith('*'):
+            continue
+        src, desc, _ = line.rstrip().split()
+        weight = random.randint(1,184)
+        temp = [src, desc, str(weight)]
+        f.write(" ".join(temp) + "\n")
